51da43e86c9c1ff541ee11ecee796ec6df35e1e3
i9 <oluwarinolasam@gmail.com>
2024-09-28 11:14:06 +0100

    removed sql db helpers as we want to see if it can be replaced with the mongodb version

--- helpers/dbHelpers.go
+++ helpers/dbHelpers.go
@@ -1,86 +1,26 @@
 package helpers
 
-import (
-	"context"
-	"errors"
-	"i9rfs/server/appGlobals"
-
-	"github.com/jackc/pgx/v5"
-)
-
 func QueryRowField[T any](sql string, params ...any) (*T, error) {
-	rows, _ := appGlobals.DBPool.Query(context.Background(), sql, params...)
 
-	res, err := pgx.CollectOneRow(rows, pgx.RowToAddrOf[T])
-	if err != nil {
-		return nil, err
-	}
-
-	return res, err
+	return nil, nil
 }
 
 func QueryRowsField[T any](sql string, params ...any) ([]*T, error) {
-	rows, _ := appGlobals.DBPool.Query(context.Background(), sql, params...)
 
-	res, err := pgx.CollectRows(rows, pgx.RowToAddrOf[T])
-	if err != nil {
-		if errors.Is(err, pgx.ErrNoRows) {
-			return make([]*T, 0), nil
-		}
-		return nil, err
-	}
-
-	return res, nil
+	return nil, nil
 }
 
 func QueryRowType[T any](sql string, params ...any) (*T, error) {
-	rows, _ := appGlobals.DBPool.Query(context.Background(), sql, params...)
 
-	res, err := pgx.CollectOneRow(rows, pgx.RowToAddrOfStructByNameLax[T])
-	if err != nil {
-		if errors.Is(err, pgx.ErrNoRows) {
-			return nil, nil
-		}
-		return nil, err
-	}
-
-	return res, nil
+	return nil, nil
 }
 
 func QueryRowsType[T any](sql string, params ...any) ([]*T, error) {
-	rows, _ := appGlobals.DBPool.Query(context.Background(), sql, params...)
 
-	res, err := pgx.CollectRows(rows, pgx.RowToAddrOfStructByNameLax[T])
-	if err != nil {
-		if errors.Is(err, pgx.ErrNoRows) {
-			return nil, nil
-		}
-		return nil, err
-	}
-
-	return res, nil
+	return nil, nil
 }
 
 func BatchQuery[T any](sqls []string, params [][]any) ([]*T, error) {
-	var res = make([]*T, len(sqls))
 
-	batch := &pgx.Batch{}
-
-	for i, sql := range sqls {
-		batch.Queue(sql, params[i]...).QueryRow(func(row pgx.Row) error {
-			var sr *T
-
-			if err := row.Scan(sr); err != nil {
-				return err
-			}
-
-			res[i] = sr
-
-			return nil
-		})
-	}
-
-	s_err := appGlobals.DBPool.SendBatch(context.Background(), batch).Close()
-
-	return res, s_err
+	return nil, nil
 }
