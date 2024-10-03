983202615af98b6c2993d9c5e4b905519dd2f625
the-real-i9 <oluwarinolasam@gmail.com>
2024-10-04 00:10:05 +0100

    better AccountExists model

--- models/appModel/appModel.go
+++ models/appModel/appModel.go
@@ -2,26 +2,28 @@ package appModel
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"i9rfs/server/appGlobals"
 	"log"
 	"time"
 
 	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
 func AccountExists(emailOrUsername string) (bool, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	count, err := appGlobals.DB.Collection("user").CountDocuments(ctx, bson.M{"$or": bson.A{bson.M{"email": emailOrUsername}, bson.M{"username": emailOrUsername}}})
+	err := appGlobals.DB.Collection("user").FindOne(ctx, bson.M{"$or": bson.A{bson.M{"email": emailOrUsername}, bson.M{"username": emailOrUsername}}}).Decode(&struct{}{})
 	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return false, nil
+		}
+
 		log.Println(fmt.Errorf("appModel.go: AccountExists: %s", err))
 		return false, appGlobals.ErrInternalServerError
-	}
-
-	if count == 0 {
-		return false, nil
 	}
 
 	return true, nil
