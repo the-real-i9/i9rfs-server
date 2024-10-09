a0f8de3a8bfe8594c4401901247f03ce733a0331
the-real-i9 <oluwarinolasam@gmail.com>
2024-10-09 22:20:08 +0100

    enhanced pg error output

--- models/rfsCmdModel/rfsCmdModel.go
+++ models/rfsCmdModel/rfsCmdModel.go
@@ -1,16 +1,21 @@
 package rfsCmdModel
 
 import (
+	"errors"
 	"fmt"
 	"i9rfs/server/appGlobals"
 	"i9rfs/server/helpers"
 	"log"
+
+	"github.com/jackc/pgx/v5/pgconn"
 )
 
 func PathExists(path string) (bool, error) {
 	exists, err := helpers.QueryRowField[bool]("SELECT EXISTS(SELECT 1 FROM directory WHERE path = $1)", path)
 	if err != nil {
-		log.Println(fmt.Errorf("rfsCmdModel.go: PathExists: %s", err))
+		var pgErr *pgconn.PgError
+		errors.As(err, &pgErr)
+		log.Println(fmt.Errorf("rfsCmdModel.go: PathExists: %s: %s", pgErr.Message, pgErr.Detail))
 		return false, appGlobals.ErrInternalServerError
 	}
 
@@ -20,7 +25,9 @@ func PathExists(path string) (bool, error) {
 func Mkdir(parentDirPath string, newDirTree []string, userId string) (bool, error) {
 	_, err := helpers.QueryRowField[bool]("SELECT mkdir($1, $2, $3)", parentDirPath, newDirTree, userId)
 	if err != nil {
-		log.Println(fmt.Errorf("rfsCmdModel.go: Mkdir: %s", err))
+		var pgErr *pgconn.PgError
+		errors.As(err, &pgErr)
+		log.Println(fmt.Errorf("rfsCmdModel.go: Mkdir: %s: %s", pgErr.Message, pgErr.Detail))
 		return false, appGlobals.ErrInternalServerError
 	}
 
@@ -35,7 +42,9 @@ type cmdDBRes struct {
 func Rmdir(dirPath string) (bool, error) {
 	res, err := helpers.QueryRowType[cmdDBRes]("SELECT status, err_msg FROM rmdir($1)", dirPath)
 	if err != nil {
-		log.Println(fmt.Errorf("rfsCmdModel.go: Rmdir: %s", err))
+		var pgErr *pgconn.PgError
+		errors.As(err, &pgErr)
+		log.Println(fmt.Errorf("rfsCmdModel.go: Rmdir: %s: %s", pgErr.Message, pgErr.Detail))
 		return false, appGlobals.ErrInternalServerError
 	}
 
@@ -49,7 +58,9 @@ func Rmdir(dirPath string) (bool, error) {
 func Rm(fsObjectPath string, recursive bool) (bool, error) {
 	res, err := helpers.QueryRowType[cmdDBRes]("SELECT status, err_msg FROM rm($1, $2)", fsObjectPath, recursive)
 	if err != nil {
-		log.Println(fmt.Errorf("rfsCmdModel.go: Rm: %s", err))
+		var pgErr *pgconn.PgError
+		errors.As(err, &pgErr)
+		log.Println(fmt.Errorf("rfsCmdModel.go: Rm: %s: %s", pgErr.Message, pgErr.Detail))
 		return false, appGlobals.ErrInternalServerError
 	}
 
